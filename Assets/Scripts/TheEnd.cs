918f0c9ee864af6c01f8c45b36924ae19d5fb970
peto123455 <peto123455@gmail.com>
2020-12-06 20:08:04 +0100

    202012062007

---
+++ Assets/Scripts/TheEnd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TheEnd : MonoBehaviour
+{
+    void Awake()
+    {
+        PlayerPrefs.SetInt("level", 0);
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
