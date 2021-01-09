2de7e2678a003af0e8158221bbebc9dc7e00c1d6
peto123455 <peto123455@gmail.com>
2021-01-09 21:26:18 +0100

    Added version checker

--- Assets/Scripts/GlobalValues.cs
+++ Assets/Scripts/GlobalValues.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public static class GlobalValues
 {
     public const byte WEAPONS_COUNT = 4;
+    public const byte version = 1;
+    public const string versionText = "V0.2a";
     public static byte difficulty;
     public static byte fov;
 }
---
+++ Assets/Scripts/VersionCheck.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class VersionCheck : MonoBehaviour
+{
+    [SerializeField] private GameObject version, state;
+    private Text versionText, stateText;
+
+    void Awake()
+    {
+        versionText = version.GetComponent<Text>();
+        stateText = state.GetComponent<Text>();
+
+        versionText.text = GlobalValues.versionText;
+        StartCoroutine(CheckVersion());
+    }
+
+    public IEnumerator CheckVersion()
+    {
+        using(UnityWebRequest webRequest = UnityWebRequest.Get("http://13.49.138.187/version.php")) //Odosielam web request na môj veb, ktorý overí kľúč
+        {
+            webRequest.timeout = 10;
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                state.SetActive(true);
+                stateText.text = "Version check error !";
+                stateText.color = Color.red;
+            }
+            else if(GlobalValues.version  < int.Parse(webRequest.downloadHandler.text)) //Authenticate(true, autoLogin);
+            {
+                state.SetActive(true);
+                stateText.text = "There is a new\nupdate available !";    
+                stateText.color = Color.yellow;      
+            }
+            else
+            {
+                state.SetActive(true);
+                stateText.text = "Up to date !";
+                stateText.color = Color.green;
+            }
+        }
+    }
+}
